677242aa50e9a9af349ed7552910c9dacf74cc9c
Alost <ketleysouza55@gmail.com>
2024-05-21 13:26:18 -0300

    Box Servidores
    Feito a Section dos servidores.

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.showModal();
+    } else {
+        console.error('Modal não encontrado:', modalId);
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.close();
+    } else {
+        console.error('Modal não encontrado:', modalId);
+    }
+}
