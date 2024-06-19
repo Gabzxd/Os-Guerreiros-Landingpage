92cfbbe4d962f91be0235847e2e67efaadbf10dd
Alost <junio.ribeiro02@hotmail.com>
2024-06-18 22:19:45 -0300

    Feito a home page & sidebar

---
+++ script.js
@@ -0,0 +1,12 @@
+const sidebar = document.getElementById("mySidebar");
+const sidebarBtn = document.getElementById("sideButton");
+const sideOpen = document.getElementById("openSide");
+
+function openSide(){
+    sidebar.style.display = 'flex';
+}
+
+function closeSide(){
+    sidebar.style.display = 'none';
+}
+
